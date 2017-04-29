e80ba339a6375cbe1d1e5bdcb2ee20432421268c
山本憲 <em072029@gmail.com>
2017-04-29 10:21:13 +0900

    Oauth認証ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-resources :users,contraints: {id: /\d+/}
+  resources :users,contraints: {id: /\d+/}
+  get '/auth/:provider/callback' => 'sessions#create',via: [:get,:post]
+  get '/logout' => 'sessions#destroy', as: :logout
 
 end
 
