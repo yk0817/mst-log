49685f114521f2e55810e8572343176ca390e819
山本憲 <em072029@gmail.com>
2017-04-29 17:23:49 +0900

    テーブル定義修正、タイムライントゥートビュー粗、ユーザーモデルテーブルカラム追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[index show]
   def index
     #code
   end
+  
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
