fde554d75e2ee923126ff5b23d52ee529e815b75
山本憲 <em072029@gmail.com>
2017-05-01 20:51:59 +0900

    タイムヘルパー

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def time_parse(input_date)
+    if Time.now.to_i - input_date.to_i < 60 * 60 #1h以内か
+      "#{((Time.now.to_i - input_date.to_i)/60).round}分"
+    elsif Time.now.to_i - input_date.to_i < 60 * 60 * 24 #1日前か
+      "#{((Time.now.to_i - input_date.to_i)/60/60).round}時間"
+    else
+      input_date.strftime("%m月%d日")
+    end
+  end
 end
