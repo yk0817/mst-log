bf29ce45358712e30d0b3445c60f6b726c471545
山本憲 <em072029@gmail.com>
2017-05-02 00:21:37 +0900

    メモ：Activerecordで書くのは諦めて生sqlで叩くことにした。

--- app/models/toot.rb
+++ app/models/toot.rb
@@ -1,4 +1,14 @@
 class Toot < ApplicationRecord
   # select date(toot_date) as date,count(*) from toots where user_id = 1 AND  toot_date BETWEEN (CURDATE() - INTERVAL 7 DAY) AND (CURDATE() + INTERVAL 1 DAY) group by date ;
-  scope :week_count, ->(user_id) { where("created_at < ?", time) }
+  # scope :week_count, ->(user_id) { where("created_at < ?", time) }
+  
+  def toot_week_count(user_id)
+    con = ActiveRecord::Base.connection
+    sql = <<'EOS'
+      select date(toot_date) as date,count(*) from toots where user_id = #{user_id} AND  toot_date
+      BETWEEN (CURDATE() - INTERVAL 7 DAY) AND (CURDATE() + INTERVAL 1 DAY) group by date ;
+    EOS
+    result = con.select_all('SELECT name, email FROM users')
+  end
+
 end
