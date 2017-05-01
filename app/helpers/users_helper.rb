3d5adbc190bc5a9e872960bcef3d75ebf596f26c
山本憲 <em072029@gmail.com>
2017-05-01 22:00:17 +0900

    sql 作成中

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
   def link_display(url)
     url.html_safe unless url.nil? 
   end
+  
+  def boosted?(boost)
+    "<i class=\"fa fa-retweet fa-2\"></i>".html_safe if boost == 1
+  end
+  
+  def date_comprise
+  end
+  
 end
