afcc111fe00674e76520e9f529beb045900adae4
山本憲 <em072029@gmail.com>
2017-05-02 23:41:25 +0900

    モデル化　collectionで回す

--- app/models/toot.rb
+++ app/models/toot.rb
@@ -4,12 +4,12 @@ class Toot < ApplicationRecord
   def toot_week_count(*num)
     
 sql=<<EOS
-  select date(toot_date) as date,count(*) from toots where user_id = #{num.first}
+  select date(toot_date) as date,count(*) as count from toots where user_id = #{num.first}
   AND  toot_date BETWEEN (CURDATE() - INTERVAL 21 DAY)
   AND (CURDATE() + INTERVAL 1 DAY) group by date 
 EOS
-
-    ActiveRecord::Base.connection.select_all(sql)
+  
+    ActiveRecord::Base.connection.select_all(sql).to_hash
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users,contraints: {id: /\d+/}
+  resources :users
   get '/auth/:provider/callback' => 'sessions#create',via: [:get,:post]
   get '/logout' => 'sessions#destroy', as: :logout
 
