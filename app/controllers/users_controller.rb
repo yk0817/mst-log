7b76134795735e379a41ecec88a2a310784311aa
山本憲 <em072029@gmail.com>
2017-05-03 01:37:01 +0900

    Kaminari導入　カスタマイズまだ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[index show]
   before_action :set_toot, only: %i[show]
+  PER = 10
+  PER.freeze
   
   def index
     # 一旦仮置き本来はユーザーIDが入ってきたらrelation入れて億
@@ -9,7 +11,6 @@ class UsersController < ApplicationController
   
   def show    
     @date_counts = Toot.new.toot_week_count(1)
-    
   end
   
   private
@@ -19,7 +20,9 @@ class UsersController < ApplicationController
   end
   
   def set_toot
-    @toots = @user.toots.limit(10)
+    # @toots = @user.toots
+    @toots = @user.toots.page(params[:page]).per(PER)
+
   end
   
 end
