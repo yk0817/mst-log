c1ee6b08fb7c441d33dd895a5b15c3b68569f080
山本憲 <em072029@gmail.com>
2017-05-07 04:18:30 +0900

    Iconリンク作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,8 @@ module ApplicationHelper
   def instance_link(user,instance_name)
     instance_count = @user.crawl_states.where(:instance => instance_name).count
     account = @user.crawl_states.find_by(:instance => instance_name)
-    link_to_if(instance_count >= 1 ,"#{account.instance.to_s}","https://#{account.instance}/@#{account.instance_user_name}",:class => "#{instance_name}-link")
+    # link_to_if(instance_count >= 1 ,"#{account.instance.to_s}","https://#{account.instance}/@#{account.instance_user_name}",:class => "#{instance_name}-link")
+    link_to_if(instance_count >= 1 ,image_tag("#{account.instance.to_s}.png",:class=> "instance-icon"),"https://#{account.instance}/@#{account.instance_user_name}",:class => "#{instance_name}-link")
   end
   
 
