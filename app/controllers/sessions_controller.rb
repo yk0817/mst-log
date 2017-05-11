50d7b769bbd125d3cc0fc2d5ac23ea01faa8548f
山本憲 <em072029@gmail.com>
2017-05-11 11:22:00 +0900

    パス変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
     user = User.find_or_create_user_auth_hash(request.env['omniauth.auth'])
     session[:user_id] = user.id
     # redirect処理→、更新フラグがある場合とルート変更したい
-    redirect_to(users_path)
+    redirect_to(user_path(current_user.id))
   end
   
   def destroy
     reset_session
-    redirect_to(users_path)
+    redirect_to(home_path)
   end
   
 end
