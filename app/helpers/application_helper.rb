de8624c70b1eca65192b78fb9dcd1b012faf0158
山本憲 <em072029@gmail.com>
2017-05-12 18:14:15 +0900

    error 削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,9 +14,11 @@ module ApplicationHelper
   end
   
   def instance_link(user,instance_name)
-    instance_count = @user.crawl_states.where(:instance => instance_name).count || 0
-    account = @user.crawl_states.find_by(:instance => instance_name)
-    link_to_if(instance_count >= 1 ,image_tag("#{account.instance.to_s}.png",:class=> "instance-icon"),"https://#{account.instance}/@#{account.instance_user_name}",:class => "#{instance_name}-link")
+    if CrawlState.where(:user_id => user.id).exists?
+      instance_count = user.crawl_states.where(:instance => instance_name).count 
+      account = user.crawl_states.find_by(:instance => instance_name)
+      link_to_if(instance_count >= 1 ,image_tag("#{account.instance.to_s}.png",:class=> "instance-icon"),"https://#{account.instance}/@#{account.instance_user_name}",:class => "#{instance_name}-link")    
+    end
   end
   
 
