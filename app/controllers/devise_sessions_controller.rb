b188f3721405fb887c6181e34c622b236706362d
山本憲 <em072029@gmail.com>
2017-05-21 16:10:29 +0900

    devise用　オーバーライドコントローラー作成

---
+++ app/controllers/devise_sessions_controller.rb
@@ -0,0 +1,13 @@
+class DeviseSessionsController < Devise::SessionsController
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+
+  def destroy
+    super
+  end
+end
---
+++ test/controllers/devise_sessions_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DeviseSessionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
