5e9f0aa56331b4964e0c4c86378e7c96f644aae6
山本憲 <em072029@gmail.com>
2017-05-22 21:41:34 +0900

    メソッドがながいので少し分離

--- app/models/crawl_state.rb
+++ app/models/crawl_state.rb
@@ -3,8 +3,15 @@ class CrawlState < ApplicationRecord
   has_many  :toots, dependent: :destroy,foreign_key: 'crawl_instance_id'
   
   def self.relate_toot_find_destroy(instance,account_params={})
+    hash = hash_for_save_instance(instance,account_params)
+    if CrawlState.where(hash).count == 0 && hash[:instance_user_name] != ""
+      CrawlState.where(:instance => hash[:instance],:user_id => hash[:user_id]).all.destroy_all
+      CrawlState.create(hash)      
+    end
+  end
+  
+  def self.hash_for_save_instance(instance,account_params)
     hash = {}
-    
     hash[:user_id] = account_params["user_id"].to_i    
     if instance.to_s.match(/other_instance/)
       hash[:instance] = account_params["#{instance}"]
@@ -16,10 +23,7 @@ class CrawlState < ApplicationRecord
       hash[:instance] = instance
       hash[:instance_user_name] = account_params["#{instance}"].gsub(/^[@＠]/,"")
     end
-    if CrawlState.where(hash).count == 0 && hash[:instance_user_name] != ""
-      CrawlState.where(:instance => hash[:instance],:user_id => hash[:user_id]).all.destroy_all
-      CrawlState.create(hash)      
-    end
+    return hash
   end
 
 end
