fcd49bf10e89013629cad7453f17c994c4ea159f
山本憲 <em072029@gmail.com>
2017-05-24 15:40:27 +0900

    プロフ画面修正中

--- db/migrate/20170519021015_add_devise_to_users.rb
+++ db/migrate/20170519021015_add_devise_to_users.rb
@@ -2,8 +2,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: true, default: ""
+      t.string :encrypted_password, null: true, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -13,7 +13,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
+      t.integer  :sign_in_count, default: 0, null: true
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
@@ -35,8 +35,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :email,                unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
