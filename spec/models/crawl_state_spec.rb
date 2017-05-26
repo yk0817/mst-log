85a95bc6e4acb17dcf0a1aff414941e39fc1e593
山本憲 <em072029@gmail.com>
2017-05-26 17:34:25 +0900

    文面修正

--- spec/models/crawl_state_spec.rb
+++ spec/models/crawl_state_spec.rb
@@ -4,7 +4,7 @@ describe  do
   it "add 2 an 1 to make 3" do
     expect(2 + 1).to eq 3
   end
-  # 
+  
   it "test_hash_for_save_instance" do
     hash = {}
     hash[:instance] = "mstdn.jp"
@@ -23,6 +23,8 @@ describe  do
     account_params = {"user_id" => 20,"other_instance1_domain" => "mangadon.net","other_instance1" => "kenyama"}
     expect(CrawlState.hash_for_save_instance("other_instance1",account_params)).to eq hash
   end
+  
+  
 end
 
 
