c4687430cd20d24d570bb93d161701d514020fe4
山本憲 <em072029@gmail.com>
2017-05-26 17:35:33 +0900

    投稿数

--- app/models/toot.rb
+++ app/models/toot.rb
@@ -4,12 +4,14 @@ class Toot < ApplicationRecord
 sql=<<EOS
   select date(toot_date) as date,count(*) as count from toots where user_id = #{user_id.first}
   AND  toot_date BETWEEN (CURDATE() - INTERVAL 21 DAY)
-  AND (CURDATE() + INTERVAL 1 DAY) group by date order by date desc LIMIT 7
+  AND (CURDATE() + INTERVAL 1 DAY) group by date order by date desc LIMIT 5
 EOS
 
     ActiveRecord::Base.connection.select_all(sql).to_hash
   end
   
-
+  def toot_count_sort_instance(instance,user_id)
+    Toot.where(:instance => instance,:user_id => user_id).count  
+  end
 
 end
