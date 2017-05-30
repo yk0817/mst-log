687041597fb7e17cd992632202f946d0e6124127
山本憲 <em072029@gmail.com>
2017-05-30 18:26:56 +0900

    current_user deviseのテスト　一旦コミット　まだ未完成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,8 +7,6 @@ module UsersHelper
     "<i class=\"fa fa-retweet fa-2\"></i>".html_safe if boost == 1
   end
   
-  def date_comprise
-  end
   
   def have_instance_account?(instance,current_user)
     if CrawlState.where(:instance => instance ,:user_id => current_user.id).exists?
---
+++ spec/helpers/user_test_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
+
+
+RSpec.configure do |c|
+  c.include UsersHelper
+  c.include Devise::TestHelpers, type: :controller
+end
+
+RSpec.describe "user_helper" do
+  
+  before do
+    sign_in user_instance
+  end
+  it "test_boosted?" do
+    expect(boosted?(1)).to eq "<i class=\"fa fa-retweet fa-2\"></i>"
+  end
+  
+  # 未完成です。
+  it "test_have_instance_account?" do
+    allow(helper).to receive(:current_user).and_return(user)
+    expect(helper.have_instance_account?("mstdn.jp",current_user)).to eq "Bis_dri_drei"
+  end
+  
+  
+end
+  
